203fadd791e03e4522d9ea668bf0f95b628b18fc
wangke839 <839513605@qq.com>
2019-11-12 12:54:52 +0800

    初始化提交

---
+++ src/main/java/com/wangke/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.wangke.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+	@RequestMapping("list.do")
+	public String list (Model m){
+		m.addAttribute("test","测试代码");
+		return "test";
+	}
+}
